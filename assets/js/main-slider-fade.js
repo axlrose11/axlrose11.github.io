2a21e682f5c01a3a843696317249a23675558887
Phuoc Hoang <ph@PTH.local>
2019-07-17 21:43:01 -0400

    add flexslider

--- assets/js/main-slider-fade.js
+++ assets/js/main-slider-fade.js
@@ -1,5 +1,5 @@
 	'use strict';
-	
+
 	// MAIN SLIDER-FLEXSLIDER
 	//==================================================================================
 	$(window).load(function() {
@@ -8,32 +8,46 @@
 			animation : "fade",
 			controlNav: false,
 			directionNav: true,
-			slideshowSpeed: 5000,  
+			slideshowSpeed: 5000,
 			animationSpeed:1000,
 			pauseOnAction: false,
-			animationLoop: true, 
-			pauseOnHover: true,			
+			animationLoop: true,
+			pauseOnHover: false,
 		});
 	});
-	
-	
-	// Check FIREFOX 
+
+
+	// Check FIREFOX
 	//----------------------------------------------------------------------------------
 	var is_firefox = function is_firefox() {
 		if (navigator.userAgent.toLowerCase().indexOf('firefox') > -1){
 			return 1;
 		}
 	}
-	
+
 	// Check IE11 (Not Windows Phone)
 	///----------------------------------------------------------------------------------
-	var isIE11desktop = function isIE11desktop() {	
+	var isIE11desktop = function isIE11desktop() {
  		if (!!navigator.userAgent.match(/Trident\/7\./) && window.navigator.userAgent.indexOf("Windows Phone") < 0) {
    		 	return 1;
 		}
 	}
-	
+
 	if (is_firefox() || isIE11desktop()){
 		$('.flexslider .slides').addClass("css-fade");
 	}
-	
+
+	// 02. FULLSCREEN CLASS
+	//==================================================================================
+	// var fullscreen = function(){
+	// 	var fheight = $(window).height();
+	// 	$('.fullscreen').css("height",fheight);
+	// }
+
+	//Execute on load
+	// fullscreen();
+	//
+	// //Execute on window resize
+	// $(window).resize(function() {
+	// 	fullscreen();
+	// });
